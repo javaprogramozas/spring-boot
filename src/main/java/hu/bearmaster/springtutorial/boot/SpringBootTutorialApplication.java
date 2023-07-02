e4b1ed0a765a95cb63a91419f1c1ad33b7bd3b8e
javaprogramozas <javaprogramozas.hu@gmail.com>
2023-07-02 18:35:35 +0200

    [v40] Spring Boot alkalmazás - I. rész

--- src/main/java/hu/bearmaster/springtutorial/boot/SpringBootTutorialApplication.java
+++ src/main/java/hu/bearmaster/springtutorial/boot/SpringBootTutorialApplication.java
@@ -3,8 +3,12 @@ package hu.bearmaster.springtutorial.boot;
 import hu.bearmaster.springtutorial.boot.model.UserContext;
 import hu.bearmaster.springtutorial.boot.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -13,8 +17,20 @@ import java.util.Optional;
 @SpringBootApplication
 public class SpringBootTutorialApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootTutorialApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTutorialApplication.class, args);
+		//SpringApplication.run(SpringBootTutorialApplication.class, args);
+		new SpringApplicationBuilder()
+				.sources(SpringBootTutorialApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.lazyInitialization(true)
+				.listeners(event -> logEvent(event))
+				.run(args);
+	}
+
+	private static void logEvent(ApplicationEvent event) {
+		LOGGER.info("*** event: {}", event.getClass().getSimpleName());
 	}
 
 	@Bean
