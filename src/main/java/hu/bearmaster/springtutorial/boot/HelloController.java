f0acd2f15253797196fcb4be9c6594ad52023062
javaprogramozas <javaprogramozas.hu@gmail.com>
2023-07-02 19:17:14 +0200

    [v41] Spring Boot alkalmazás - II. rész

--- src/main/java/hu/bearmaster/springtutorial/boot/HelloController.java
+++ src/main/java/hu/bearmaster/springtutorial/boot/HelloController.java
@@ -1,19 +1,35 @@
 package hu.bearmaster.springtutorial.boot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.CommandLinePropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HelloController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Value("${hello}")
+    private List<String> helloArgs;
+
+    @Value("${" + CommandLinePropertySource.DEFAULT_NON_OPTION_ARGS_PROPERTY_NAME + "}")
+    private List<String> nonOptionArgs;
+
     @GetMapping("/hello")
     public String hello(Model model) {
+        LOGGER.info("Hello {}", helloArgs);
+        LOGGER.info("Hello2 {}", nonOptionArgs);
         Integer userCount = jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
         model.addAttribute("count", userCount);
         return "hello";
--- src/main/java/hu/bearmaster/springtutorial/boot/SpringBootTutorialApplication.java
+++ src/main/java/hu/bearmaster/springtutorial/boot/SpringBootTutorialApplication.java
@@ -5,6 +5,7 @@ import hu.bearmaster.springtutorial.boot.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,6 +13,7 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -31,6 +33,19 @@ public class SpringBootTutorialApplication {
 
 	private static void logEvent(ApplicationEvent event) {
 		LOGGER.info("*** event: {}", event.getClass().getSimpleName());
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return arguments -> {
+			LOGGER.info("Raw arguments: {}", (Object) arguments.getSourceArgs());
+			LOGGER.info("Option names: {}", arguments.getOptionNames());
+			if (arguments.containsOption("hello")) {
+				List<String> helloArguments = arguments.getOptionValues("hello");
+				LOGGER.info("Hello {}", helloArguments);
+			}
+			LOGGER.info("Non-options: {}", arguments.getNonOptionArgs());
+		};
 	}
 
 	@Bean
